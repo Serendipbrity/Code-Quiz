9057cf905279f9730093b4f923f472ec03d96615
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-03-29 19:24:19 -0400

    current

--- js/script.js
+++ js/script.js
@@ -6,15 +6,17 @@
 // css hover change color
 var startBtn = document.getElementById("startBtn");
 let questions = document.querySelector(".questions");
-var questionOne = document.querySelector("#q1");
-var questionTwo = document.querySelector("#q2");
-var questionThree = document.querySelector("#q3");
+var questionOne = document.getElementById("q1");
+var questionTwo = document.getElementById("q2");
+var questionThree = document.getElementById("q3");
 var allQuestions = [questionOne, questionTwo, questionThree];
 console.log(allQuestions);
 var currentQuestion = 0;
+var next = document.getElementById("next");
 //  function playQuiz() {
 //     var buttons = document.querySelector.all()
 //  }
+
 
 // timer starts at 2 minutes
 const startingMinutes = 2;
@@ -23,7 +25,7 @@ let time = startingMinutes * 60;
 
 const countdownEl = document.getElementById('countdown');
 
-
+// when start button clicked, hide start page, show first question
 startBtn.addEventListener("click", function(event) {
 var startScreen = document.querySelector(".start");
 startScreen.classList.add("hide");
@@ -32,7 +34,8 @@ startScreen.classList.remove("show");
 questions.classList.add("show");
 questions.classList.remove("hide");
 
-    setInterval(updateCountdown, 1000);
+// timer
+ setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
     const minutes = Math.floor(time / 60);
@@ -44,85 +47,82 @@ function updateCountdown() {
         time--; 
         time = time < 0 ? 0: time;
     }
-
-    getQuestion();
-    console.log("button was clicked");
 });
 
-function getQuestion() {
-    console.log(currentQuestion);
+ next.addEventListener("click", function(){getQuestion()});
+
+
+    console.log("here");
   
-    if (currentQuestion > 0) {
-      allQuestions[currentQuestion - 1].setAttribute("class", "hide");
-    }
-    console.log(allQuestions[currentQuestion]);
+ 
+  function getQuestion() { 
+    for(var i = 0; i < 2; i++);
+     {
+        allQuestions[currentQuestion + 1].classList.remove('.hide');
+        allQuestions[currentQuestion + 1].classList.add('.show');
+        console.log(currentQuestion);
+     }
   
-    allQuestions[currentQuestion].setAttribute("class", "show");
+    allQuestions[currentQuestion].classList.remove('.show');
+    allQuestions[currentQuestion].classList.add('.hide');
   
-    playQuiz();
-  }
+    playQuiz(currentQuestion[i++]);
+    
+    
+    console.log("try");
+};
   
+
+// function getQuestion() {
+//     console.log(currentQuestion);
+  
+//     if (currentQuestion >= 0) {
+//       allQuestions[currentQuestion + 1].setAttribute("class", "show");
+//     }
+//     console.log(allQuestions[currentQuestion]);
+  
+//     allQuestions[currentQuestion].setAttribute("class", "hide");
+  
+//     playQuiz();
+//   }
+
   function playQuiz() {
-    document.querySelector(`q.${currentQuestion + 1}-answers`).addEventListener('click', function(event) {
+    //document.querySelector(`questions.${currentQuestion - 1}-answers`).addEventListener('click', function(event) {
+         document.querySelector(".answers").addEventListener("click", function(event){
+
       var btnClicked = event.target;
   
       console.log(btnClicked);
-  
+        });
       // check if question is correct or now
   
       // manage timer
   
       currentQuestion++;
   
-      if (currentQuestion < allQuestions.length) {
-        getQuestion();
-      } else {
-        questions.classList.add('hide');
-        questions.classList.remove('show');
-    
-        var endScreen = document.querySelector('.end');
-        endScreen.classList.add('show');
-        endScreen.classList.remove('hide');
-      }
-    })
-  };
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+      };
   
 
 // function getQuestion() {
 //     for(var i = 0; i < 3; i++);
 // }
 
-// function getQuestion() { 
-//     if (currentQuestion > 0) {
-//         allQuestions[currentQuestion - 1].classList.remove('show');
-//         allQuestions[currentQuestion - 1].classList.add('remove');
-//       }
+
+
   
-//     allQuestions[currentQuestion].classList.remove('hide');
-//     allQuestions[currentQuestion].classList.add('show');
-  
-//     playQuiz(currentQuestion[i++]);
-  
-//     currentQuestion++;
-//     getQuestion.addEventListener
-//   }
-  
-//   function playQuiz() {
-//    currentQuestion++;
-//    getQuestion();
-//   }
+  function playQuiz() {
+   currentQuestion++;
+   getQuestion();
+
+   if (currentQuestion < allQuestions.length) {
+    getQuestion();
+  } else {
+    questions.classList.add('hide');
+    questions.classList.remove('show');
+
+    var endScreen = document.querySelector('.end');
+    endScreen.classList.add('show');
+    endScreen.classList.remove('hide');
+}
+  }
+
