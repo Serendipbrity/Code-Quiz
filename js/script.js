766a8af87a655c251e7d1786dc4bf472b80741a2
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-03-30 14:03:28 -0400

    update

--- js/script.js
+++ js/script.js
@@ -13,17 +13,7 @@ var allQuestions = [questionOne, questionTwo, questionThree];
 console.log(allQuestions);
 var currentQuestion = 0;
 var next = document.getElementById("next");
-//  function playQuiz() {
-//     var buttons = document.querySelector.all()
-//  }
 
-
-// timer starts at 2 minutes
-const startingMinutes = 2;
-// multiply by 60 (seconds)
-let time = startingMinutes * 60;
-
-const countdownEl = document.getElementById('countdown');
 
 // when start button clicked, hide start page, show first question
 startBtn.addEventListener("click", function(event) {
@@ -34,7 +24,14 @@ startScreen.classList.remove("show");
 questions.classList.add("show");
 questions.classList.remove("hide");
 
+
+
+// timer starts at 2 minutes
+const startingMinutes = 2;
+// multiply by 60 (seconds)
+let time = startingMinutes * 60;
 // timer
+const countdownEl = document.getElementById('countdown');
  setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
@@ -48,81 +45,72 @@ function updateCountdown() {
         time = time < 0 ? 0: time;
     }
 });
-
+// next button click to get next question
  next.addEventListener("click", function(){getQuestion()});
 
 
     console.log("here");
   
- 
+ // show next question, hide last
   function getQuestion() { 
-    for(var i = 0; i < 2; i++);
+    for(var i = 0; i < 3; i++);
+    
      {
-        allQuestions[currentQuestion + 1].classList.remove('.hide');
-        allQuestions[currentQuestion + 1].classList.add('.show');
-        console.log(currentQuestion);
+      allQuestions[currentQuestion].classList.remove('show');
+      allQuestions[currentQuestion].classList.add('hide');
+     
      }
   
-    allQuestions[currentQuestion].classList.remove('.show');
-    allQuestions[currentQuestion].classList.add('.hide');
+     currentQuestion = allQuestions[currentQuestion + 1].classList.remove('hide');
+     currentQuestion = allQuestions[currentQuestion + 1].classList.add('show');
+     console.log(currentQuestion);
   
-    playQuiz(currentQuestion[i++]);
+     currentQuestion = i;
     
     
     console.log("try");
 };
   
-
-// function getQuestion() {
-//     console.log(currentQuestion);
-  
-//     if (currentQuestion >= 0) {
-//       allQuestions[currentQuestion + 1].setAttribute("class", "show");
-//     }
-//     console.log(allQuestions[currentQuestion]);
-  
-//     allQuestions[currentQuestion].setAttribute("class", "hide");
-  
-//     playQuiz();
-//   }
+var correctAnswers = document.value("true");
+console.log(correctAnswers);
 
   function playQuiz() {
     //document.querySelector(`questions.${currentQuestion - 1}-answers`).addEventListener('click', function(event) {
          document.querySelector(".answers").addEventListener("click", function(event){
 
       var btnClicked = event.target;
-  
+
+      // if (currentQuestion < allQuestions.length) {
+      //   currentQuestion++;
+      //       // getQuestion();
+      //     } else {
+      //       questions.classList.add('hide');
+      //       questions.classList.remove('show');
+      //     }
       console.log(btnClicked);
         });
       // check if question is correct or now
   
       // manage timer
   
-      currentQuestion++;
+      // currentQuestion++;
   
       };
-  
 
-// function getQuestion() {
-//     for(var i = 0; i < 3; i++);
+  
+//   function playQuiz() {
+//    currentQuestion++;
+//    getQuestion();
+
+//    if (currentQuestion < allQuestions.length) {
+//     getQuestion();
+//   } else {
+//     questions.classList.add('hide');
+//     questions.classList.remove('show');
+
+//     var endScreen = document.querySelector('.end');
+//     endScreen.classList.add('show');
+//     endScreen.classList.remove('hide');
 // }
-
-
-
-  
-  function playQuiz() {
-   currentQuestion++;
-   getQuestion();
-
-   if (currentQuestion < allQuestions.length) {
-    getQuestion();
-  } else {
-    questions.classList.add('hide');
-    questions.classList.remove('show');
-
-    var endScreen = document.querySelector('.end');
-    endScreen.classList.add('show');
-    endScreen.classList.remove('hide');
-}
-  }
+//   }
 
